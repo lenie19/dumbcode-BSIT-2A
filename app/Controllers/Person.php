59c3321762273a0b4653a27e1853e7e4ba5bbce9
jmadvincula200616@gmail.com <gpsychogirl12@gmail.com>
2026-02-22 19:46:14 +0800

    //

--- app/Controllers/Person.php
+++ app/Controllers/Person.php
@@ -74,23 +74,6 @@ class Person extends Controller
     }
 }
 
-public function delete($id){
-    $model = new PersonModel();
-    $logModel = new LogModel();
-    $user = $model->find($id);
-    if (!$user) {
-        return $this->response->setJSON(['success' => false, 'message' => 'Person not found.']);
-    }
-
-    $deleted = $model->delete($id);
-
-    if ($deleted) {
-        $logModel->addLog('Delete user', 'DELETED');
-        return $this->response->setJSON(['success' => true, 'message' => 'Person deleted successfully.']);
-    } else {
-        return $this->response->setJSON(['success' => false, 'message' => 'Failed to delete Person.']);
-    }
-}
 
 public function fetchRecords()
 {
--- app/Controllers/Profiling.php
+++ app/Controllers/Profiling.php
@@ -78,24 +78,6 @@ class Profiling extends Controller
     }
 }
 
-public function delete($id){
-    $model = new ProfilingModel();
-    $logModel = new LogModel();
-    $user = $model->find($id);
-    if (!$user) {
-        return $this->response->setJSON(['success' => false, 'message' => 'Profiling not found.']);
-    }
-
-    $deleted = $model->delete($id);
-
-    if ($deleted) {
-        $logModel->addLog('Delete Profiling', 'DELETED');
-        return $this->response->setJSON(['success' => true, 'message' => 'Profiling deleted successfully.']);
-    } else {
-        return $this->response->setJSON(['success' => false, 'message' => 'Failed to delete Profiling.']);
-    }
-}
-
 public function fetchRecords()
 {
     $request = service('request');
